98c71ebe751ccd08e5c6d20004ad84fd7d40ce2a
mhtsharma948 <mhtsharma948@gmail.com>
2017-12-02 16:45:59 +0530

    Updated config

--- config.js
+++ config.js
@@ -4,6 +4,6 @@ config.gremlinEndpoint = "<GREMLIN ENDPOINT>";
 config.documentEndpoint = "<DOCUMENT ENDPOINT>";
 config.primaryKey = "<PRIMARY KEY>";
 config.database = "<DATABASE NAME>"
-config.collection = "<COLLENCTION NAME>"
+config.collection = "<COLLECTION NAME>"
 
 module.exports = config;
